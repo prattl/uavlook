bae96a8c522aea6a12f7fcec7a99f294aabdad41
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-03-16 23:39:41 -0400

    Add migration for header title fields

--- uav/home/models.py
+++ uav/home/models.py
@@ -8,6 +8,7 @@ from wagtail.wagtailcore.fields import (
 )
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel,
+    FieldRowPanel,
     MultiFieldPanel,
     StreamFieldPanel
 )
@@ -15,14 +16,23 @@ from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
+    heading_1_title = models.CharField('Left Heading Title', max_length=128, null=True)
+    heading_2_title = models.CharField('Center Heading Title', max_length=128, null=True)
+    heading_3_title = models.CharField('Right Heading Title', max_length=128, null=True)
     description = RichTextField(blank=True)
-    body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
-    ], null=True)
+
+    # body = StreamField([
+    #     ('heading', blocks.CharBlock(classname="full title")),
+    #     ('paragraph', blocks.RichTextBlock()),
+    #     ('image', ImageChooserBlock()),
+    # ], null=True)
 
     content_panels = Page.content_panels + [
+        FieldRowPanel([
+            FieldPanel('heading_1_title'),
+            FieldPanel('heading_2_title'),
+            FieldPanel('heading_3_title'),
+        ]),
         FieldPanel('description', classname="full"),
-        StreamFieldPanel('body'),
+        # StreamFieldPanel('body'),
     ]
