d4cb216cf6be83207ceb9ce163a56587898b4baf
Lenny Pratt <lenny@prattdev.net>
2017-03-16 20:50:19 -0400

    Testing wagtail features

--- uav/home/models.py
+++ uav/home/models.py
@@ -1,9 +1,28 @@
-from __future__ import absolute_import, unicode_literals
-
 from django.db import models
 
+from wagtail.wagtailcore import blocks
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import (
+    RichTextField,
+    StreamField,
+)
+from wagtail.wagtailadmin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    StreamFieldPanel
+)
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
-    pass
+    description = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full"),
+        StreamFieldPanel('body'),
+    ]
