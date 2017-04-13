ca400431b098700b623a369737b0f3c555200dc3
Lenny Pratt <lenny@prattdev.net>
2017-04-12 22:56:24 -0400

    Display form validation errors

--- uav/home/templatetags/custom.py
+++ uav/home/templatetags/custom.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter
 def get_key_value(some_dict, key):
     return some_dict.get(key, '')
+
+
+@register.filter
+def get_form_field(form, key):
+    return getattr(form, key, '')
+
