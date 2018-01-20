9fdcb1d3d6c8cbcb30f83cbc3f094e4fc6faa836
Lenny Pratt <lenny.pratt@hireanesquire.com>
2018-01-20 17:05:25 -0500

    Use https url

--- uav/uav/settings/base.py
+++ uav/uav/settings/base.py
@@ -146,6 +146,6 @@ WAGTAIL_SITE_NAME = "uav"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://uavlook.com'
+BASE_URL = 'https://uavlook.com'
 
 
